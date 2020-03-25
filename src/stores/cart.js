76156fe7304022b86c7f8438d3c050137c24f880
binh.trinh <binh.trinh@titancorpvn.com>
2020-03-25 17:13:28 +0700

    update

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -6,8 +6,8 @@ const initialState = {
 
 export const actionCreators = {
     getCarts: (cartList) => ({type: 'FETCH_CARTS', cartList}),
-    addCart: (cart) => ({type: 'ADD_CART', cart}),
-    removeCart: (cartId) => ({type: 'REMOVE_CART', cartId})
+    addToCart: (cart) => ({type: 'ADD_TO_CART', cart}),
+    removeFromCart: (cartId) => ({type: 'REMOVE_FROM_CART', cartId})
 }
 
 export const reducer = (state, action) => {
@@ -18,12 +18,12 @@ export const reducer = (state, action) => {
                 ...state,
                 cartList: action.cartList
             }
-        case 'ADD_CART':
+        case 'ADD_TO_CART':
             return {
                 ...state,
                 cartList: [state.cartList, action.cart]
             }
-        case 'REMOVE_CART':
+        case 'REMOVE_FROM_CART':
             let _cartList = _.get(state, 'cartList')
             let filterList = _cartList.filter((f) => f.id !== action.cartId)
             return {
